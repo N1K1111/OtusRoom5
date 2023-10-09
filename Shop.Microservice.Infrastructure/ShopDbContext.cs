35b94b6e07205d5ea76fd6261d8eee09595ba2a5
N1K1111 <nik.glazkov2016@yandex.ru>
2023-10-09 13:58:59 +0300

    Add DbContext in Shop.Infrustructure

---
+++ Shop.Microservice.Infrastructure/ShopDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Microservice.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Microservice.Infrastructure
+{
+    internal class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
+
+        public DbSet <Balance> Balances { get; set;}
+        public DbSet <Order> Orders { get; set;}
+        public DbSet <Product> Products { get; set;}
+        public DbSet <Transaction> Transactions { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+}
---
+++ Shop.Microservice.Infrastructure/ShopDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Shop.Microservice.Infrastructure
+{
+    internal class ShopDbContextFactory : IDesignTimeDbContextFactory<ShopDbContext>
+    {
+        public ShopDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ShopDbContext>();
+            var connectionString = @"Host=localhost;Port=5432;Database=otus_glazev_test;Username=postgres;Password=123123";
+            optionsBuilder.UseNpgsql(connectionString);
+            return new ShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
